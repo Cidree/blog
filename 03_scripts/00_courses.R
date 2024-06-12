d1b5e463d16848785a9c5b3d98bd9a9810fe6831
Cidree <adrian.cidre@gmail.com>
2024-06-12 23:41:23 +0200

    improve course banners

--- 03_scripts/00_courses.R
+++ 03_scripts/00_courses.R
@@ -22,31 +22,68 @@ course_card <- function(file, course_title, mode, date, time, language, href) {
 }
 
 ## Course banner -----------------------------------------------------------
-course_banner <- function(price, mode, time, date, img_file) {
+course_banner <- function(price, mode, time, date, img_file, lang = "es") {
   
   div(
     img(src = img_file, class = "course-banner--image"),
+    ## Icons
     div(
       class = "grid text-center",
       style = "vertical-align: bottom",
       div(
         class = "g-col-3",
-        icon("money-bill-1", class = "fa-2xl course-banner--icon"),
+        icon("euro-sign", class = "fa-2xl course-banner--icon")
+      ),
+      div(
+        class = "g-col-3",
+        icon("chalkboard-user", class = "fa-2xl course-banner--icon")
+      ),
+      div(
+        class = "g-col-3",
+        icon("clock", class = "fa-2xl course-banner--icon")
+      ),
+      div(
+        class = "g-col-3",
+        icon("calendar-days", class = "fa-2xl course-banner--icon")
+      )
+    ),
+    ## Titles
+    div(
+      class = "grid text-center",
+      div(
+        class = "g-col-3",
+        p(if (lang == "es") "Precio" else "Price", class = "course-banner--title")
+      ),
+      div(
+        class = "g-col-3",
+        p(if (lang == "es") "Modalidad" else "Mode", class = "course-banner--title")
+      ),
+      div(
+        class = "g-col-3",
+        p(if (lang == "es") "Duración" else "Time", class = "course-banner--title")
+      ),
+      div(
+        class = "g-col-3",
+        p(if (lang == "es") "Fecha" else "Date", class = "course-banner--title")
+      )
+    ),
+    ## Characteristics
+    div(
+      class = "grid text-center",
+      div(
+        class = "g-col-3",
         p(price, class = "course-banner--text")
       ),
       div(
         class = "g-col-3",
-        icon("chalkboard-user", class = "fa-2xl course-banner--icon"),
         p(mode, class = "course-banner--text")
       ),
       div(
         class = "g-col-3",
-        icon("clock", class = "fa-2xl course-banner--icon"),
         p(time, class = "course-banner--text")
       ),
       div(
         class = "g-col-3",
-        icon("calendar-days", class = "fa-2xl course-banner--icon"),
         p(date, class = "course-banner--text")
       )
     )
